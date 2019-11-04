f86da0fd52586235c65eae8ddbdaf0f55f1420c5
Ro-oR <56259005+Ro-oR@users.noreply.github.com>
2019-11-04 15:42:54 +0100

    Update Aufgabenblatt

--- ortlepp/ro_grundlagen_javascript.js
+++ ortlepp/ro_grundlagen_javascript.js
@@ -1,53 +1,3 @@
-<<<<<<< HEAD
-
-function sayHello(name) {
-
-    //Gibt Inhalt auf Konsole wieder
-    console.log('Hello ' + name);
-}
-
-//Führt die Funktion aus
-sayHello('Robin')
-
-
-//Zufällige Bewertungen für vorgegebene Anzahl an Bewertungen
-const bewertungMax = 5
-var anzahl = 15
-var i =1
-while(i <= anzahl) {
-    //Math.random gibt eine zufällig Zahl zwischen 0 und 1 aus.
-    //Multipliziert mit der gewünschten Maximalgröße lässt sich mit
-    //math.floor der entsprechende Int-Wert ausgeben
-    //Nachteil: Keine 0 möglich!!
-    var bewertung = Math.floor(Math.random() * bewertungMax) +1
-    console.log("Bewertung Nummer:", i)
-    console.log("Bewertung: ",bewertung)
-    i++
-}
-
-
-
-const bewertungMax = 5
-var anzahl = 15
-var readline = require('readline');
-var bewertung = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-bewertung.question('Was ist die Bewertung?', function(answer){
-    if(answer<=bewertungMax){
-        console.log('Die eingegebene Bewertung ist:', answer)
-        var bew = answer
-        bewertung.close();
-    }
-    else{
-        console.log("Die eingegebene Bewertung ist nicht korrekt")
-        bewertung.close();
-    }
-
-=======
-
 function sayHello(name) {
 
     //Gibt Inhalt auf Konsole wieder
@@ -72,7 +22,6 @@ while(i <= anzahl) {
     console.log("Bewertung: ",bewertung)
     i++
 }
-
 */
 
 const bewertungMax = 5
@@ -82,7 +31,7 @@ var bewertung = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
+/*
 bewertung.question('Was ist die Bewertung?', function(answer){
     if(answer<=bewertungMax){
         console.log('Die eingegebene Bewertung ist:', answer)
@@ -93,6 +42,45 @@ bewertung.question('Was ist die Bewertung?', function(answer){
         console.log("Die eingegebene Bewertung ist nicht korrekt")
         bewertung.close();
     }
-
->>>>>>> ff846da14090ae22bb26d6d84272fad7b16c9f57
 })
+*/
+
+//Aufgabe 4:
+
+function getRandom(min, max) {
+	return Math.floor(Math.random() * (max - min +1)) + min; 
+  } 
+console.log('\n')
+
+
+bewertung.question('Wie oft soll bewertet werden?:', function(answer) {
+    if(isNaN(answer)) {
+        console.log("Keine gülitge Eingabe!!")
+        bewertung.close();
+    }
+    else{
+        a=1
+        //console.log("Keine gülitge Eingabe!!")
+        var gesamt =0 
+	    for(i=0; i<answer; i++){
+	        var x = getRandom(0, bewertungMax)
+	        console.log('\nDies ist Bewertung Nummer '+(i+1))
+		    console.log ("Die Bewertung ist:" +x)
+		    anzahl++
+	        gesamt = gesamt + x
+	        console.log("Gesamtzahl der Bewertungen:" +anzahl )
+		    console.log("Gesamtbewertung:"+(gesamt/anzahl))
+        }
+        bewertung.close();
+    }
+})
+
+
+console.log('\n')
+
+let bewArray = [];
+
+bewArray.push(bewertung);
+bewArray.push(anzahl);
+console.log("Das ist die letzte eingetragene Bewertung:",bewArray[0])
+console.log("Das ist die Anzahl der Bewertungen:", bewArray[1])
