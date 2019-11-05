8ac8a282c09608b6a8a64774fb48802a9f25eeea
Ro-oR <robin.ortlepp@gmx.de>
2019-11-05 17:58:29 +0100

    Update Aufgabe 2

--- ortlepp/ro_grundlagen_javascript.js
+++ ortlepp/ro_grundlagen_javascript.js
@@ -1,3 +1,5 @@
+async function main() {
+
 function sayHello(name) {
 
     //Gibt Inhalt auf Konsole wieder
@@ -24,13 +26,7 @@ while(i <= anzahl) {
 }
 */
 
-const bewertungMax = 5
-var anzahl = 15
-var readline = require('readline');
-var bewertung = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+
 /*
 bewertung.question('Was ist die Bewertung?', function(answer){
     if(answer<=bewertungMax){
@@ -44,43 +40,64 @@ bewertung.question('Was ist die Bewertung?', function(answer){
     }
 })
 */
-
 //Aufgabe 4:
 
-function getRandom(min, max) {
-	return Math.floor(Math.random() * (max - min +1)) + min; 
-  } 
-console.log('\n')
 
+async function randomBew() {
 
-bewertung.question('Wie oft soll bewertet werden?:', function(answer) {
-    if(isNaN(answer)) {
-        console.log("Keine gülitge Eingabe!!")
-        bewertung.close();
-    }
-    else{
-        a=1
-        //console.log("Keine gülitge Eingabe!!")
-        var gesamt =0 
-	    for(i=0; i<answer; i++){
-	        var x = getRandom(0, bewertungMax)
-	        console.log('\nDies ist Bewertung Nummer '+(i+1))
-		    console.log ("Die Bewertung ist:" +x)
-		    anzahl++
-	        gesamt = gesamt + x
-	        console.log("Gesamtzahl der Bewertungen:" +anzahl )
-		    console.log("Gesamtbewertung:"+(gesamt/anzahl))
+    const bewertungMax = 5
+    var anzahl = 0
+    let bewArray = [];
+    var readline = require('readline');
+
+    var bewertung = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    function getRandom(min, max) {
+	    return Math.floor(Math.random() * (max - min +1)) + min; 
+    } 
+
+    bewertung.question('Wie oft soll bewertet werden?:', function(answer) {
+        if(isNaN(answer)) {
+            console.log("Keine gülitge Eingabe!!")
+            bewertung.close();
         }
-        bewertung.close();
-    }
-})
+        else{
+            var gesamt =0 
+	        for(i=0; i<answer; i++){
+                var x = getRandom(0, bewertungMax)
+	            console.log('\nDies ist Bewertung Nummer '+(i+1))
+		        console.log ("Die Bewertung ist:" +x)
+		        anzahl++
+	            gesamt = gesamt + x
+	            console.log("Gesamtzahl der Bewertungen:" +anzahl )
+                console.log("Gesamtbewertung:"+(gesamt/anzahl))
+            }
+
+            //Hier werden nun die KOmponenten Name, letzte Bewertung und Anzahl der Bewertungen in ein Array eingefügt
+            bewArray.push(i)
+            bewArray.push(x)
+            bewArray.push(anzahl)
+            //und schlie0lich ausgegeben
+            console.log("Array-Ausgabe:")
+            console.log("\n\nBewertungsname: "+bewArray[0],"\nAktuelle Bewertung: "+bewArray[1],"\nAnzahl der Bewertungen: "+bewArray[2])
+
+            //Hier werden die o.g. Komponenten in ein Objekt eingefügt
+            let ratings = {i,x,anzahl};
+            console.log("Object-Ausgabe:")
+            console.log("\n\n"+ratings)
+            bewertung.close();
+         }
+    })
+}
+
+await randomBew()
+
 
 
 console.log('\n')
 
-let bewArray = [];
-
-bewArray.push(bewertung);
-bewArray.push(anzahl);
-console.log("Das ist die letzte eingetragene Bewertung:",bewArray[0])
-console.log("Das ist die Anzahl der Bewertungen:", bewArray[1])
+}
+main()
