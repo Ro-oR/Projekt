df678ad542fc0f9dd0432400dd813a8f2bd46fcb
unknown <waeschephilipp@gmail.com>
2019-12-02 12:12:11 +0100

    Test

--- Waesche/main.js
+++ Waesche/main.js
@@ -7,9 +7,14 @@ var u2 = new User("Test2", "test2@test");
 var u3 = new User("Test3", "test3@test");
 
 function testfun1(){
-    var request = new XMLHttpRequest()//.withCredentials(apitoken)
-    request.open("GET", "https://api.deutschebahn.com/freeplan/v1/location/BLN", false)
+    var request = new XMLHttpRequest()
+    request.open("GET", "https://api.deutschebahn.com/freeplan/v1/location/KGU", false)
+    var suche = "Gummersbach"
+    request.withCredentials = true
+    //request.open("GET", "https://api.deutschebahn.com/stada/v2/stations?searchstring="+suche, false)
+    request.setRequestHeader("Authorization: Bearer 3d67bf9773fba69970a84b25e1ae9b3d")
     request.send(null)
+    console.log(request)
     return request.responseText
 }
 
