b60aedbb3ef6e689f9513c0ec1c672d6a9e61a42
unknown <waeschephilipp@gmail.com>
2019-12-02 11:27:33 +0100

    .

--- Waesche/main.js
+++ Waesche/main.js
@@ -1,4 +1,5 @@
 const User = require("./user.js");
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;//Installieren via "npm i xmlhttprequest"
 
 var apitoken = "3d67bf9773fba69970a84b25e1ae9b3d"
 var u1 = new User("Test1", "test1@test");
@@ -6,7 +7,7 @@ var u2 = new User("Test2", "test2@test");
 var u3 = new User("Test3", "test3@test");
 
 function testfun1(){
-    var request = new XMLHttpRequest()
+    var request = new XMLHttpRequest()//.withCredentials(apitoken)
     request.open("GET", "https://api.deutschebahn.com/freeplan/v1/location/BLN", false)
     request.send(null)
     return request.responseText
