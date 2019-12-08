f5f8bfc41f7cd7dbeaa79b5b63974aea3d1cc3c4
unknown <waeschephilipp@gmail.com>
2019-12-08 23:35:41 +0100

    .

--- Waesche/functions.js
+++ Waesche/functions.js
@@ -1,6 +1,9 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;//Installieren via "npm i xmlhttprequest"
 var apitoken = "3d67bf9773fba69970a84b25e1ae9b3d"
 
+var XMLparser = require('fast-xml-parser');//npm install fast-xml-parser
+var util = require("util")
+
 function umlautCheck(str){
     str = str.replace("ä", "%C3%A4")
     str = str.replace("ö", "%C3%B6")
@@ -33,6 +36,28 @@ module.exports = {
         request.open("GET", "https://api.deutschebahn.com/timetables/v1/plan/"+bahnhofsID+"/"+datum+"/"+stunde, false)
         request.setRequestHeader("Authorization", "Bearer " + apitoken)
         request.send()
+
+        var options = {
+            attributeNamePrefix : "@_",
+            attrNodeName: "attr", //default is 'false'
+            textNodeName : "#text",
+            ignoreAttributes : true,
+            ignoreNameSpace : false,
+            allowBooleanAttributes : false,
+            parseNodeValue : true,
+            parseAttributeValue : false,
+            trimValues: true,
+            cdataTagName: "__cdata", //default is 'false'
+            cdataPositionChar: "\\c",
+            localeRange: "", //To support non english character in tag/attribute values.
+            parseTrueNumberOnly: false,
+            arrayMode: false, //"strict"
+            attrValueProcessor: (val, attrName) => he.decode(val, {isAttributeValue: true}),//default is a=>a
+            tagValueProcessor : (val, tagName) => he.decode(val), //default is a=>a
+            stopNodes: ["parse-me-as-string"]
+        };
+
+        console.log(util.inspect(XMLparser.parse(request.responseText, options)))
         return request.responseText
     }
 }
