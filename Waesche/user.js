320a0a0c21aa389b00bc9bff3d872a0d187faee9
unknown <waeschephilipp@gmail.com>
2019-12-11 20:02:42 +0100

    .

--- Waesche/user.js
+++ Waesche/user.js
@@ -1,3 +1,4 @@
+const functions = require("./functions.js")
 const Ticket = require("./ticket.js");
 const RegionalTicket = require("./ticket.js");
 const fs = require("fs");
@@ -19,12 +20,25 @@ function userCount(){
     return count.UserCount
 }
 
+function angebotListe(){
+    var angebote = JSON.parse(fs.readFileSync('./angebote.json', 'utf8', (err) => {
+            if (err) {
+                console.log("Lesefehler", err)
+                return
+            }
+        })
+    )
+    return angebote
+}
+
 class User{
     constructor(username, contact){
         this.userID = userCount()
         this.username = username
         this.contact = contact //Handynummer/Email
         this.tickets = new Array
+        this.angebote = new Array
+        this.suchen = new Array
     }
     addTicket(bereich){
         var t = new Ticket(this.userID, bereich)
@@ -35,6 +49,18 @@ class User{
         var t = new RegionalTicket(this.userID, bereich)
         this.tickets.push(t.ticketID)
         console.log(t.owner + " " + this.tickets)
+    }
+    addAngebot(fahrtstrecke){
+        this.angebote.push(fahrtstrecke)
+        var neuesAngebot = {
+            anbieter : this.userID,
+            strecke : fahrtstrecke
+        }
+        var alleAngebote = angebotListe()
+        alleAngebote.push(neuesAngebot)
+        fs.writeFileSync('./angebote.json', JSON.stringify(alleAngebote), err => {
+            if (err) { console.log("Schreibfehler", err) }
+        })
     }
 }
 
